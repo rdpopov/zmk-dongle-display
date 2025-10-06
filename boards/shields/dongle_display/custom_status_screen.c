33e8829f72941f81902e6845019baf36f27278be
Rosen Popov <50305462+rdpopov@users.noreply.github.com>
2025-10-07 00:06:39 +0300

    Update

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -91,6 +91,7 @@ lv_obj_t *zmk_display_status_screen() {
     #if IS_ENABLED(CONFIG_ZMK_BATTERY)
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
     lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
+    #endif
 #endif
 
     return screen;
